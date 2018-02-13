23c7597e61a0cc6fc6ba47492d1eee7b6640f488
Alessandro Vatri <avatri@turing.ac.uk>
2018-02-13 02:36:11 +0000

    TT lexicon generates from training set
    alpha

--- Scripts/TT_training_create_lexicon.py
+++ Scripts/TT_training_create_lexicon.py
@@ -1,23 +1,24 @@
-import re
 import os
-from grkLemmata import greekLemmata
-
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.system("clear && printf '\e[3J'")
+source=open('TreeTaggerData/training_set.txt','r').read()
+dictionary = {}
+def create_dictionary(word, pos):
+	dictionary.setdefault(word,set()).add(pos)
+	
+lines = source.split("\n")
+for line in lines:
+	form = line.split("\t")[0]
+	pos = line.split("\t")[1]
+	create_dictionary(form,pos)
 
-start_dict = open('grkFrm.py', 'r').read()
-print('Bear with me a sec...')
-newDict = start_dict.replace('greekForms = {\n','')
-newDict = newDict.replace(',\n','\n')
-newDict = re.sub(", 'a': \['.*?'\]", "", newDict)
-newDict = re.sub("{'l': '(.*?)'}", r"\1", newDict)
-newDict = re.sub("\[(.*?)\]", r"\1", newDict)
-newDict = newDict.replace(', ', '\t')
-newDict = newDict.replace('":', '\t')
-newDict = newDict.replace('"', '')
-newDict = newDict.replace(',', '')
-newDict = newDict.replace('\n}','')
-newDict = re.sub('\t((nlsj)?\d+)', lambda x : '\t'+greekLemmata[x.group(1)]['pos']+'\t'+x.group(1), newDict)
-newDict += "\n.\tSENT\t.\n:\tSENT\t:\n;\tSENT\t;\n?\tSENT\t;"
-open('TreeTaggerData/lexicon.txt', 'w').write(newDict)
-print('All done')
+dictionary['.']={'SENT'}
+dictionary[';']={'SENT'}
+dictionary[':']={'SENT'}
+dictionary['?']={'SENT'}
+dictionary[',']={','}
+
+dictiostring = ''
+for entry in dictionary.items():
+	dictiostring += '%s\t%s\t-\n'%(entry[0],'\t-\t'.join([b for b in entry[1]]))
+open('TreeTaggerData/lexicon.txt','w').write(dictiostring.strip())
