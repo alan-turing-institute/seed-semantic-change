d9b422698e1f9c9163fd5375b57408c956c09a52
Alessandro Vatri <avatri@turing.ac.uk>
2018-02-13 22:47:55 +0000

    TT ancient Greek parameters!
    plus new training set stuff

--- Scripts/TT_training.py
+++ Scripts/TT_training.py
@@ -7,4 +7,4 @@ os.system("python3 %s/TT_training_create_lexicon.py" % (os.path.dirname(os.path.
 config = configparser.ConfigParser()
 config.read('config.ini')
 treetagger = config['paths']['treetagger']
-os.system("{tt_folder}/train-tree-tagger {curr_folder}/TreeTaggerData/lexicon.txt {curr_folder}/TreeTaggerData/openclass.txt {curr_folder}/TreeTaggerData/training_set.txt {curr_folder}/TreeTaggerData/ancient_greek.txt".format(tt_folder=treetagger, curr_folder=os.path.dirname(os.path.realpath(__file__))))
+os.system("{tt_folder}/train-tree-tagger {curr_folder}/TreeTaggerData/lexicon.txt {curr_folder}/TreeTaggerData/openclass.txt {curr_folder}/TreeTaggerData/training_set.txt {curr_folder}/TreeTaggerData/ancient_greek.dat".format(tt_folder=treetagger, curr_folder=os.path.dirname(os.path.realpath(__file__))))
