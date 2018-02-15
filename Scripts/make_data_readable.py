73f0cfe341d594c37cce1962b9d806124a121d81
Alessandro Vatri <avatri@turing.ac.uk>
2018-02-15 17:33:35 +0000

    added function to translate ids into forms in output of asLemmata_onefile.py

--- Scripts/make_data_readable.py
+++ Scripts/make_data_readable.py
@@ -1,9 +1,13 @@
 import re
+import os
 from grkLemmata import greekLemmata
 file = input('Data file to decode: ').strip().replace('\\ ', ' ')
 data = open(file, 'r').read()
 print('Processing')
-processed_data = re.sub('((nlsj)?\d+)( \(.*?\) ;)', lambda m : '%s [%s]%s'%(greekLemmata[m.group(1)]['lemma'], m.group(1), m.group(3)), data)
+if os.path.splitext(file)[1] == '.dat':
+	processed_data = re.sub('((nlsj)?\d+)( \(.*?\) ;)', lambda m : '%s [%s]%s'%(greekLemmata[m.group(1)]['lemma'], m.group(1), m.group(3)), data)
+else:
+	processed_data = re.sub('(?<=[ \t])(nlsj)?\d+',lambda m : greekLemmata.get(m.group(0),{'lemma':m.group(0)})['lemma'], data)
 output = open(file+'_decoded.txt', 'w')
 output.write(processed_data)
 output.close()
