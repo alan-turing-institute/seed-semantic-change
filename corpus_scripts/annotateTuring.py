98de9250d661b378231d99660ee25ed277e62449
Alessandro Vatri <avatri@turing.ac.uk>
2018-02-22 19:08:26 +0000

    renamed folder, bug fixes, script to format corpus for TreeTagger

--- corpus_scripts/annotateTuring.py
+++ corpus_scripts/annotateTuring.py
@@ -2,3 +2,4 @@ import os
 os.system("python3 %s/tokenizePerseus.py" % (os.path.dirname(os.path.realpath(__file__))))
 os.system("python3 %s/tokenizeConverted.py" % (os.path.dirname(os.path.realpath(__file__))))
 os.system("python3 %s/corporaParser.py" % (os.path.dirname(os.path.realpath(__file__))))
+os.system("python3 %s/TT_format_our_data.py" % (os.path.dirname(os.path.realpath(__file__))))
