c9a72cc485161fafcc71805300c2a5279c81924f
Alessandro Vatri <avatri@turing.ac.uk>
2018-03-08 17:03:43 +0100

    updates

--- corpus_scripts/annotate_senses.py
+++ corpus_scripts/annotate_senses.py
@@ -65,12 +65,13 @@ def sense_in_file():
 					except:
 						continue
 				print('Input sense: "%s"'%sense_glosses[int(i_s)-1])
-				sentence.xpath('./word/lemma[@id="%s"][not(@sense)]'%word_id)[0].set('sense', sense_ids[int(i_s)-1])
 				while True:
-					notes = input('How did it go?\n\t1 - no problem\n\t2 - too little context\n\t3 - unclear context\n\t4 - wrong lemmatization')
+					notes = input('How did it go? (1 - no problem ; 2 - too little context ; 3 - unclear context ; 4 - wrong lemmatization) ')
 					if int(notes) in range(1,5):
 						break
+				print()
 				sentence.xpath('./word/lemma[@id="%s"][not(@sense)]'%word_id)[0].set('notes', notes)
+				sentence.xpath('./word/lemma[@id="%s"][not(@sense)]'%word_id)[0].set('sense', sense_ids[int(i_s)-1])
 				parse.write(current_file, xml_declaration = True, encoding='UTF-8', pretty_print=True)
 
 #extract target word data
