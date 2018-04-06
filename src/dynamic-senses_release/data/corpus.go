2128a96dd149e4fb90eb2a51a9b53fa7362efc52
Valerio Perrone <valerio.perrone@carloalberto.org>
2018-04-06 14:13:55 +0100

    Update corpus.go

--- src/dynamic-senses_release/data/corpus.go
+++ src/dynamic-senses_release/data/corpus.go
@@ -46,7 +46,7 @@ func NewDocument() (doc *Document){
 }
 
 
-/* Randomly initialized document labels of a corpus 
+/* Randomly initialized document labels of a corpus
  *   Seed < 1 means no randomization */
 func (corpus *Corpus) InitializeLabeling(numTop int, seed int) {
   rand.Seed(int64(seed))
@@ -77,13 +77,13 @@ func (corpus *Corpus) RandomizeOrder(seed int) {
    Output binary corps with all 'documents' for each target concept. Time-tagged. */
 func Create_corpus_from_text(txt_corpus_path, target_concept_path string, window_size int) (corpus *Corpus) {
 
-    
+
     corpus = NewCorpus()
-    
+
     /* read text corpus */
     txt_lines, err := util.Read_lines(txt_corpus_path)
     if err !=nil {panic(err)}
-    
+
     /* create dictionary of target words */
     target_words, err := util.Read_lines(target_concept_path)
     target_word_dict  := make(map[string]int, len(target_words))
@@ -91,14 +91,14 @@ func Create_corpus_from_text(txt_corpus_path, target_concept_path string, window
         target_word_dict[word] = 0
     }
     if err !=nil {panic(err)}
-    
+
     /* fill corpus with target-word specific, time-stamped documents */
     for _, line := range(txt_lines) {
         if len(strings.Split(line, "\t")) == 2 {
             year_s, text := strings.Split(line, "\t")[0], strings.Split(line, "\t")[1]
             year, err    := strconv.Atoi(year_s)
             if err != nil {panic(err)}
-        
+
             words        := strings.Split(text, " ")
             /* iterated through text in line and record a document whenever we find a target word */
             for wIdx:=0 ; wIdx < len(words) ; wIdx++ {
@@ -108,7 +108,7 @@ func Create_corpus_from_text(txt_corpus_path, target_concept_path string, window
                         doc.Target = corpus.TargetFeatures.Lookup(words[wIdx])
                         doc.Time   = year
                         cxt := make(map[int]int)
-                        for i:=wIdx-window_size ; i<wIdx+window_size ; i++ {
+                        for i:=wIdx-window_size ; i<wIdx+window_size+1 ; i++ {
                             if i != wIdx {
                                 cxt[corpus.ContextFeatures.Lookup(words[i])]++
                             }
@@ -122,7 +122,7 @@ func Create_corpus_from_text(txt_corpus_path, target_concept_path string, window
             }
         }
     }
-    return 
+    return
 }
 
 
@@ -138,24 +138,24 @@ func Create_corpus_from_text(txt_corpus_path, target_concept_path string, window
 func (corpus *Corpus) Extract_subcorpus(target string, slicesize, low, up, max_docs_per_slice int) ([]*Document,  *CodeMap,  *CodeMap, int) {
 
   if _,ok := corpus.TargetFeatures.StringtoID[target] ; ok {
-    
+
     time_map, max := Year_to_id(low, up, slicesize)
-        
+
     doc_count := make(map[int]int, len(time_map))
-    
+
     featuremap := &CodeMap{}
     featuremap.IDtoString = make(map[int]string)
     featuremap.StringtoID = make(map[string]int)
-    
+
     tgtfeature := &CodeMap{}
     tgtfeature.IDtoString = make(map[int]string)
     tgtfeature.StringtoID = make(map[string]int)
     tgtfeature.StringtoID[target]                        = corpus.TargetFeatures.StringtoID[target]
     tgtfeature.IDtoString[tgtfeature.StringtoID[target]] = target
-    
+
     corpus2 := make([]*Document,0)
     for _,doc := range(corpus.Documents) {
-      
+
       if _,ok := time_map[doc.Time] ; ok && doc.Target == corpus.TargetFeatures.StringtoID[target]  {
         if time_map[doc.Time] <= max {
 	new_doc := doc.Copy()
@@ -167,7 +167,7 @@ func (corpus *Corpus) Extract_subcorpus(target string, slicesize, low, up, max_d
 	if max_docs_per_slice==0 ||  doc_count[new_doc.Time] < max_docs_per_slice {
 	  corpus2 = append(corpus2, new_doc)
 	  doc_count[new_doc.Time]++
-	  
+
 	  // 	fmt.Println(doc.Time, corpus2[len(corpus2)-1].Time)
 	  for fIdx,f := range(doc.Context) {
 	    corpus2[len(corpus2)-1].Context[fIdx][0] = featuremap.Lookup(corpus.ContextFeatures.IDtoString[f[0]])
@@ -218,7 +218,7 @@ func (document *Document) String(tgtFeatures, cxtFeatures map[int]string) (strin
     string_doc += fmt.Sprint("\t")
   }
   for _,v := range(document.Context) {
-    for j:=0 ; j<v[1] ; j++ { 
+    for j:=0 ; j<v[1] ; j++ {
       fmt.Print(cxtFeatures[v[0]], " ")
       string_doc += fmt.Sprint(cxtFeatures[v[0]], " ")
     }
@@ -283,7 +283,7 @@ func Load_corpus(filename string) (crp *Corpus) {
   }
   dec := gob.NewDecoder(fh)
   err = dec.Decode(&crp)
-  
+
   if err != nil {
     fmt.Println(err)
   }
