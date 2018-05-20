46fdf7db2a5cf550e67d1a86c5383e8f4144a3f4
Valerio Perrone <valerio.perrone@carloalberto.org>
2018-05-20 12:25:06 +0100

    data extension in progress for m.lognormals_f

--- src/dynamic-senses_release/model/model.go
+++ src/dynamic-senses_release/model/model.go
@@ -18,7 +18,10 @@ type Model struct {
   //parameters
   Parameters *Model_parameters
   LogNormals_k map[int]*matrix.DenseMatrix
-  LogNormals_f map[int]*matrix.DenseMatrix
+
+  //LogNormals_f map[int]*matrix.DenseMatrix  //TODO old
+  LogNormals_f []map[int]*matrix.DenseMatrix  //TODO onew
+
   Phi        map[int]*matrix.DenseMatrix
   Psi        map[int]*matrix.DenseMatrix
   N_k        map[int]*matrix.SparseMatrix //map[int][][]int
@@ -33,7 +36,13 @@ func New_model(parameters *Model_parameters) (m *Model) {
   m.Parameters      = parameters
   m.LogNormals_k    = make(map[int]*matrix.DenseMatrix, 1)
   m.LogNormals_k[0] = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_categories)
-  m.LogNormals_f    = make(map[int]*matrix.DenseMatrix, parameters.Num_categories)
+
+  //m.LogNormals_f    = make(map[int]*matrix.DenseMatrix, parameters.Num_categories) //TODO old
+  m.LogNormals_f    = make([]map[int]*matrix.DenseMatrix, m.Parameters.Num_genres)  //TODO new
+  for idx, _ := range(m.LogNormals_f) {  //TODO new
+      m.LogNormals_f[idx] = make(map[int]*matrix.DenseMatrix, m.Parameters.Num_categories) //TODO new
+  }  //TODO new
+
   m.Phi             = make(map[int]*matrix.DenseMatrix, 1)
   m.Phi[0]          = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_categories)
   m.Psi             = make(map[int]*matrix.DenseMatrix, parameters.Num_categories)
@@ -50,7 +59,8 @@ func New_model(parameters *Model_parameters) (m *Model) {
     //m.N_k_f[g] = make(map[int]*matrix.SparseMatrix, parameters.Num_categories)
     //m.N_k_sum_f[g] = make(map[int]*matrix.SparseMatrix, parameters.Num_categories)
     for k:=0 ; k<parameters.Num_categories ; k++ {
-      m.LogNormals_f[k] = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_features)
+      //m.LogNormals_f[k] = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_features) //TODO old
+      m.LogNormals_f[g][k] = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_features) //TODO new
       m.Psi[k]          = matrix.Zeros(m.Parameters.Num_timestamps, m.Parameters.Num_features)
       m.N_k_f[k]        = matrix.ZerosSparse(m.Parameters.Num_timestamps, m.Parameters.Num_features)
       m.N_k_sum_f[k]    = matrix.ZerosSparse(m.Parameters.Num_timestamps,1)
@@ -65,14 +75,17 @@ func (m *Model) Initialize_batch(corpus *data.Corpus) {
     m.Update(doc.Label, doc.Target, doc.Context, doc.Time, 1)
   }
   for tt:=0 ; tt<m.Parameters.Num_timestamps ; tt++ {
-    for kk:=0 ; kk<m.Parameters.Num_categories ; kk++ {
-      m.Phi[0].Set(tt, kk, (m.N_k[0].Get(tt,kk)+0.01) / (m.N_sum_k[0].Get(tt,0)+float64(m.Parameters.Num_categories)*0.01))
-      for vv:=0 ; vv<m.Parameters.Num_features ; vv++ {
-	m.Psi[kk].Set(tt, vv, (m.N_k_f[kk].Get(tt,vv)+0.01) / (m.N_k_sum_f[kk].Get(tt,0)+float64(m.Parameters.Num_features)*0.01))
+    for g:=0 ; g<m.Parameters.Num_genres ; g++ {  //TODO new
+      for kk:=0 ; kk<m.Parameters.Num_categories ; kk++ {
+        m.Phi[0].Set(tt, kk, (m.N_k[0].Get(tt,kk)+0.01) / (m.N_sum_k[0].Get(tt,0)+float64(m.Parameters.Num_categories)*0.01))
+        for vv:=0 ; vv<m.Parameters.Num_features ; vv++ {
+  	m.Psi[kk].Set(tt, vv, (m.N_k_f[kk].Get(tt,vv)+0.01) / (m.N_k_sum_f[kk].Get(tt,0)+float64(m.Parameters.Num_features)*0.01))
+        }
+        //m.LogNormals_f[kk].FillRow(tt, Inverse_alt(m.Psi[kk].RowCopy(tt))) ///TODO old
+        m.LogNormals_f[g][kk].FillRow(tt, Inverse_alt(m.Psi[kk].RowCopy(tt))) ///TODO new
       }
-      m.LogNormals_f[kk].FillRow(tt, Inverse_alt(m.Psi[kk].RowCopy(tt)))
+      m.LogNormals_k[0].FillRow(tt, Inverse_alt(m.Phi[0].RowCopy(tt)))
     }
-    m.LogNormals_k[0].FillRow(tt, Inverse_alt(m.Phi[0].RowCopy(tt)))
   }
 }
 
