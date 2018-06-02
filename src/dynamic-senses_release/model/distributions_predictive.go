ebc229013594a5c6d7d747cabe661675ea01a5c1
Valerio Perrone <valerio.perrone@carloalberto.org>
2018-06-02 12:23:16 +0200

    fix loglikelihood genre

--- src/dynamic-senses_release/model/distributions_predictive.go
+++ src/dynamic-senses_release/model/distributions_predictive.go
@@ -23,16 +23,25 @@ func (m *Model) Predict(corpus *data.Corpus) (loglikelihood float64) {
     /* compute log p(d|model) */
     doc_likelihood := make([]float64, m.Parameters.Num_categories)
 
-    for g:=0 ; g<m.Parameters.Num_genres ; g++ {
+    //for g:=0 ; g<1 ; g++ {  //TODO was m.Parameters.Num_genres
       for k:=0 ; k<m.Parameters.Num_categories ; k++ {
         /* compute log p(d|k) */
-        doc_likelihood[k] += math.Log(m.Phi[g][0].Get(t_minus_1,k))
+        doc_likelihood[k] += math.Log(m.Phi[doc.Genre][0].Get(t_minus_1,k))
         for _,w := range(doc.Context) {
           doc_likelihood[k] += (math.Log(m.Psi[k].Get(t_minus_1,w[0]))) * float64(w[1])
         }
       }
-    }
+    //}
+
+    //for k:=0 ; k<m.Parameters.Num_categories ; k++ {
+    //      print("doc_likelihood[k] ", doc_likelihood[k],"\n\n")
+    //}
+    //print("LOGLIKEHOOD BEFORE ", loglikelihood,"\n\n")
+    //loglikelihood = 0
+    //print("LOGLIKEHOOD zeroed ", loglikelihood,"\n\n")
+    //print("LOGLIKEHOOD temp ", util.SumExp(doc_likelihood),"\n\n")
     loglikelihood += math.Log(util.SumExp(doc_likelihood))
+    print("LOGLIKEHOOD AFTER ", loglikelihood,"\n\n")
   }
   return
 }
