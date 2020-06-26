41c6371c8e0a0e0f09a2ac724915a733f647943f
simon.koala <simon.hengchen@gmail.com>
2020-06-26 14:58:49 +0200

    fixed

--- GASC_chapter/code/main.py
+++ GASC_chapter/code/main.py
@@ -1,31 +1,37 @@
 from utils import *
+import sys
 
-genres = ["narrative","technical"]
 
-# Creating data files
-for genre in genres:
-    print("Creating corpora for...",genre)
-    corpus_transformer(genre)
+langs = ["AG", "LA"]
 
-# Training models
-genres = ["NOT-"+genre for genre in genres]
-for genre in genres:
-    earliest = -700
-    last = 400
-    slice_length = 100
-    bins = []
-    for i in range(earliest,last+slice_length,slice_length):
-        bins.append(i)
-    pool = Pool(8)
-    pool.starmap(train_model,zip(bins,repeat(genre)))
-    pool.close()
-    pool.join()
+for lang in langs:
+    print("Dealing with",lang)
+    if lang == "AG":
+        genres = ["narrative","technical"]
+        # Creating data files
+        for genre in genres:
+            print("Creating corpora for...",genre)
+            corpus_transformer(genre,lang)
 
-get_models_stats()
+        # Training models
+        genres = ["NOT-"+genre for genre in genres]
+        for genre in genres:
+            earliest = -700
+            last = 400
+            slice_length = 100
+            bins = []
+            for i in range(earliest,last+slice_length,slice_length):
+                bins.append(i)
+            pool = Pool(8)
+            pool.starmap(train_model,zip(bins,repeat(genre),repeat(lang)))
+            pool.close()
+            pool.join()
 
-genres = ["narrative","technical"]
-for genre in genres:
-    print(genre.upper())
-    for target in target_words.keys():
-        check_target_in_models(target,genre)
-    print("\n")
+        get_models_stats()
+
+        genres = ["narrative","technical"]
+        for genre in genres:
+            print(genre.upper())
+            for target in target_words.keys():
+                check_target_in_models(target,genre,lang)
+            print("\n")
